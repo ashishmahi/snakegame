aecf2c18f503fa9e15a922e12415039b0d1941f8
ashish mahindrakar <ashishmahi46@gmail.com>
2017-12-23 12:55:26 +0530

    feature to pause game by pressing any key except arrow keys

--- server.js
+++ server.js
@@ -1,39 +1,35 @@
 const http  = require('http');
 const fs = require('fs');
+const PORT = 8000;
 const getContentType=function(file){
   let contentTypes = {
     '.js':'text/javascript',
     '.html':'text/html',
     '.css':'text/css',
-    '.jpeg':'image/jpeg',
-    '.txt':'text/plain',
-    '.pdf':'text/plain',
-    '.jpg':'image/jpg',
-    '.gif':'image/gif',
     '.ico':"image/ico"
   }
   let fileExtension = file.slice(file.lastIndexOf('.'));
   return contentTypes[fileExtension];
 }
 
+const sendResponse = function(res,content,contentType,statusCode){
+  res.statusCode = statusCode;
+  res.setHeader('Content-type',contentType);
+  res.write(content);
+}
+
 const requestHandler = function(req,res){
   let file = req.url=='/'? 'snake.html' :req.url.slice(1);
   if(fs.existsSync(file)){
-    if(req.method=="POST"){
-      req.on('data',function(text){
-        storeComment(text.toString());
-        return;
-      });
-    }
     let contentType = getContentType(file);
-    res.setHeader('content-Type',contentType);
-    res.statusCode = 200;
-    res.write(fs.readFileSync(file));
+    let content = fs.readFileSync(file);
+    sendResponse(res,content,contentType,200);
 }else{
-  res.statusCode = 404;
-  res.write('404 file not found')
+  sendResponse(res,`${file} not found`,'text/plain',404);
   }
   res.end();
 }
 const server = http.createServer(requestHandler);
-server.listen(9999);
+server.listen(PORT);
+console.log(`server is listening on port ${PORT}`);
+console.log(`open browser and type localhost:${PORT} to play game`);
--- snake.js
+++ snake.js
@@ -67,6 +67,7 @@ Snake.prototype.updateSnakePos = function(key){
     "ArrowUp":this.updateSnakePosUp,
     "ArrowLeft":this.updateSnakePosLeft
   };
+  if(!actions[key])return;
   actions[key](this);
 }
 
@@ -85,18 +86,8 @@ Snake.prototype.didSnakeEatFood = function(){
   return this.foodPosition == snakeHead;
 }
 
-Snake.prototype.isGameOver = function () {
-  let maxPossiblePos = this.sizeOfGrid*this.sizeOfGrid;
-  let head = this.snakePositions.slice(-1);
-  let didSnakeHitTopEdge = head<0;
-  let didSnakeHitBottomEdge = head>(maxPossiblePos);
-  return didSnakeHitTopEdge||didSnakeHitBottomEdge
-};
 // ----------------xx module xx----------------------------
-// ---------------------------------->
-// ---------------------------------->
-// ---------------------------------->
-// ---------------------------------->
+
 
 let snake = new Snake();
 
@@ -148,10 +139,6 @@ const updateDisplayOfGame = function(){
   if(snake.didSnakeEatFood()){
     snake.snakePositions.push(snake.foodPosition);
     snake.generatedFood();
-  }
-  if(snake.isGameOver()){
-    updateDisplay("GAME OVER");
-    return;
   }
   showFood();
   updateSnakeOnDisplay();
